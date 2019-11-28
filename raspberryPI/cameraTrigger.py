6cd9ee57bfc02a81ab1b327a326d2092d26c40d4
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2019-11-27 22:07:48 -0500

    Multi Camera Compatiable + Object Oriented Class

--- raspberryPI/cameraTrigger.py
+++ raspberryPI/cameraTrigger.py
@@ -4,71 +4,97 @@ import random
 import pickle
 import os
 
+IP = ["192.168.1.113"]
+HOST = ["blueTriangle"]
+STORAGE = "F:/mocapMath/Sandbox/rpi"
+# IP = ("10.10.10.2", "10.10.10.3", "10.10.10.4", "10.10.10.5")
+
 def generateSession():
     """Generates a Random 4 Digit Hex"""
     randomInt = random.randint(4096, 65535)
     hexInt = format(randomInt, 'X')
     return hexInt
 
-def holdForString(output, checksum):
-    check = output.readline()
-
-    # Refuse Empty Lines
-    while check == '\n':
-        check = output.readline()
-
-    check = check.rstrip()
-    if check == checksum:
-        return True
-    else:
-        raise Exception
-
-def getTelemtry(output):
-    payload = output.readline()
-    
-    # Refuse Empty Lines
-    while payload == '\n':
-        payload = output.readline()
-    
-    payload = payload.rstrip()
-    payload = payload.split(" : ")
-    return payload[1]
-
-
-def sendString(inStream, payload):
-    inStream.write("{}\n".format(payload))
-    inStream.flush()
-
 # Create Session Variables
 sessionID = generateSession()
 
-# Establish SSH Connection
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect("192.168.1.113", username="pi", password="mocapMath")
+class remoteCamera():
+    def __init__(self, ipAdress):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(ipAdress, username="pi", password="mocapMath")
 
-i, o, e = ssh.exec_command("cd /home/pi/Documents; python3 cameraController.py")
+        self.i, self.o, self.e = self.ssh.exec_command("cd /home/pi/Documents; python3 cameraController.py")
 
-holdForString(o, "Session ID")
-sendString(i, sessionID)
+    def hold(self, checksum):
+        """Hold execution until remote camera returns the checksum string."""
+        check = self.o.readline()
 
-holdForString(o, "Record Delay")
-sendString(i, "1")
+        # Refuse Empty Lines
+        while check == '\n':
+            check = self.o.readline()
 
-recordedFrames = getTelemtry(o)
-print("Recorded {} Frames".format(recordedFrames))
-sharedMemoryTime = getTelemtry(o)
-print("Allocated Shared Memory in {} seconds".format(sharedMemoryTime))
-processTime = getTelemtry(o)
-print("Multi Core Processing Completed in {} seconds".format(processTime))
+        check = check.rstrip()
+        if check == checksum:
+            return True
+        else:
+            raise Exception
+    
+    def get(self):
+        """Get printout from remote camera and return the value after ' : '."""
+        payload = self.o.readline()
+    
+        # Refuse Empty Lines
+        while payload == '\n':
+            payload = self.o.readline()
+        
+        payload = payload.rstrip()
+        payload = payload.split(" : ")
+        return payload[1]
+    
+    def send(self, payload):
+        """Send payload as input string to remote client."""
+        self.i.write("{}\n".format(payload))
+        self.i.flush()
+    
+    def getFile(self, remotepath, localpath):
+        """Get file from remote client at remotepath and store it at localpath."""
+        sftp = self.ssh.open_sftp()
+        sftp.get(remotepath, localpath)
+
+# Establish Connections
+CAMERAS = []
+for client in IP:
+    connection = remoteCamera(client)
+    connection.hold("Session ID")
+    connection.send(sessionID)
+    CAMERAS.append(connection)
+
+# Prepare to Start Recording
+for camera in CAMERAS:
+    camera.hold("Record Delay")
+
+# Sync Camera Record Starts
+timeUntilStart = 1
+timeBase = time.time()
+for camera in CAMERAS:
+    camera.send(str(timeUntilStart - (time.time() - timeBase)))
+holdTime = timeUntilStart - (time.time() - timeBase)
+time.sleep(holdTime)
+print("Recording Started on Remote Cameras")
+
+# Get Telemetry
+for i, camera in enumerate(CAMERAS):
+    print("Recorded {} Frames on {}".format(camera.get(), HOST[i]))
+for i, camera in enumerate(CAMERAS):
+    print("Allocated Shared Memory in {} seconds on {}".format(camera.get(), HOST[i]))
+for i, camera in enumerate(CAMERAS):
+    print("Multi Core Processing Completed in {} seconds on {}".format(camera.get(), HOST[i]))
 
 # Retrieve Data
-filePath = getTelemtry(o)
-base = os.path.basename(filePath)
-print("Data Exported at {} on SSH Client".format(filePath))
-
-sftp = ssh.open_sftp()
-sftp.get(filePath, base)
-with open(base, "rb") as b:
-    data = pickle.load(b)
-    print("Recieved Data for {} Frames".format(len(data)))
+for camera in CAMERAS:
+    dataPath = camera.get()
+    dataFile = os.path.basename(dataPath)
+    dataLocal = os.path.join(STORAGE, dataFile)
+    camera.getFile(dataPath, dataLocal)
+    print("Recieved {}".format(dataFile))
